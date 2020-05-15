498e865c972ae8bba5c4069b5e071c6838dbccf4
marti <marti@DESKTOP-U4VTACU.lan>
2020-05-15 13:14:04 +0200

    Esercizio 1

--- src/main/java/it/polito/tdp/crimes/model/TestModel.java
+++ src/main/java/it/polito/tdp/crimes/model/TestModel.java
@@ -3,7 +3,16 @@ package it.polito.tdp.crimes.model;
 public class TestModel {
 
 	public static void main(String[] args) {
+		Model model = new Model();
+		model.creaGrafo(2, "arson");
+		System.out.println(model.numVertex());
+		System.out.println(model.numEdges());
+		System.out.println(model.output());
 		
+		model.creaGrafo(8, "murder");
+		System.out.println(model.numVertex());
+		System.out.println(model.numEdges());
+		System.out.println(model.output());
 	}
 
 }
